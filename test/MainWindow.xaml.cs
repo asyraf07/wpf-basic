297bef89eec719330827b4054bfb6a1fd33a7c7b
asyraf07 <asyraf.sr07@gmail.com>
2024-04-18 23:53:47 +0700

    initial commit

---
+++ test/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Start_Click(object sender, RoutedEventArgs e)
+        {
+            reset.IsEnabled = false;
+            for (int i = 0; i < 10; i++)
+            {
+                loding.Value += 10;
+                await Task.Delay(100);
+            }
+            reset.IsEnabled = true;
+        }
+        private async void Reset_Click(object sender, RoutedEventArgs e)
+        {
+            loding.Value = 0;
+        }
+
+    }
+}
